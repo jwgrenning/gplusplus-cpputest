c9655e3c83d5fec8b00750930218e8fcbc149288
Jon Jagger <jon@jaggersoft.com>
2020-02-03 16:11:37 +0000

    Upgrade --std to c18/c++2a

--- docker/install.sh
+++ docker/install.sh
@@ -1,21 +1,15 @@
-#!/bin/bash
+#!/bin/bash -Eeu
 
-src_install()
-{
-  apt-get update
-  cd /
-  apt-get install --yes git
-  git clone https://github.com/cpputest/cpputest.git
-  apt-get remove  --yes git
+apt-get install --yes \
+    autoconf \
+    automake \
+    git \
+    libtool
 
-  apt-get install --yes autoconf automake git libtool
-  cd cpputest
-  ./autogen.sh # This is giving errors about missing file ./ltmain.sh
-  mkdir a_build_dir
-  cd a_build_dir
-  ../configure
-  make
-  make check
-}
-
-src_install
+cd /
+git clone https://github.com/cpputest/cpputest.git
+cd cpputest/
+autoreconf . -i
+./configure
+make
+apt-get remove --yes git
