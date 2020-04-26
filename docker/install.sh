ca3a01dd57c7318b53c82de02d287e7745211a60
Jon Jagger <jon@jaggersoft.com>
2020-04-26 14:27:09 +0100

    Use latest-passing-build url in install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -7,7 +7,12 @@ apt-get install --yes \
     libtool
 
 cd /
-git clone https://github.com/cpputest/cpputest.git
+
+wget https://github.com/cpputest/cpputest/releases/download/latest-passing-build/cpputest-latest.tar.gz
+tar -xf ./cpputest-latest.tar.gz
+rm ./cpputest-latest.tar.gz
+mv cpputest-latest cpputest
+
 cd cpputest/
 autoreconf . -i
 ./configure
