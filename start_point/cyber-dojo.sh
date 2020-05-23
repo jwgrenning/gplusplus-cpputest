ceefbc7909954f2778103ae3dd497e479b470972
Jon Jagger <jon@jaggersoft.com>
2020-05-23 14:52:36 +0100

    Use more explicit tidy_up() in cyber-dojo.sh

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,12 +1,32 @@
 
+# ------------------------------------------------------------------------
+# cyber-dojo returns text files under /sandbox that are
+# created/deleted/changed. In tidy_up you can remove any
+# such files you don't want returned to the browser.
+
 trap tidy_up EXIT
+
 function tidy_up()
 {
-  # cyber-dojo returns text files under /sandbox that are
-  # created/deleted/changed. In here you can remove any
-  # such files you don't want returned to the browser.
-  [ ! -d objs/ ] || rm -rf objs/
+  delete_dirs objs/
 }
 
+function delete_dirs()
+{
+  for dirname in "$@"
+  do
+      rm -rf "${dirname}" 2> /dev/null || true
+  done
+}
+
+function delete_files()
+{
+  for filename in "$@"
+  do
+      rm "${filename}" 2> /dev/null || true
+  done
+}
+
+# ------------------------------------------------------------------------
 export CPPUTEST_HOME=/cpputest
 make
