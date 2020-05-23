1cb8bececbdbbb31406e013b2616add569c68b83
Jon Jagger <jon@jaggersoft.com>
2020-05-23 11:25:40 +0100

    Drop hidden_filenames from manifest; tidy up in cyber-dojo.sh instead

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,2 +1,12 @@
+
+trap tidy_up EXIT
+function tidy_up()
+{
+  # cyber-dojo returns text files under /sandbox that are
+  # created/deleted/changed. In here you can remove any
+  # such files you don't want returned to the browser.
+  [ ! -d objs/ ] || rm -rf objs/
+}
+
 export CPPUTEST_HOME=/cpputest
 make
